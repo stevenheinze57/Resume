9b81ccf688d355bcb388d26885ec5706a9349bdd
Steven Heinze <stevenheinze57@gmail.com>
2019-08-06 21:09:52 -0400

    Refactored the home component

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { Card } from './models/card.model';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  @Output() cardset1: Card[];
+  @Output() cardset2: Card[];
+
   constructor() { }
 
   ngOnInit() {
+    this.constructCardSets()
+  }
+
+  constructCardSets() {
+    this.cardset1 = [
+      new Card("portfolio-slide.jpeg", "Some quick example text to build on the card title and make up the bulk of the card's content."),
+      new Card("portfolio-slide.jpeg", "Download a PDF version fo my resume here!")
+    ];
+    this.cardset2 = [
+      new Card("portfolio-slide.jpeg", "Take a look at this site's source code here!"),
+      new Card("portfolio-slide.jpeg", "Learn how I automate this site's development"),
+      new Card("portfolio-slide.jpeg", "Want to hire me...?")
+    ];
   }
 
 }
---
+++ ClientApp/src/app/home/models/card.model.ts
@@ -0,0 +1,12 @@
+
+export class Card {
+
+  cardImage: string;
+  cardText: string;
+
+  constructor(cardImagePath: string, cardText: string) {
+    this.cardImage = cardImagePath;
+    this.cardText = cardText;
+  }
+
+}
