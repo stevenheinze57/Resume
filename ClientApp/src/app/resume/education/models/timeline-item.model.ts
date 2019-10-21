e4ca85aae9e560ba252e1f9d0419b6a045e4449c
Steven Heinze <stevenheinze57@gmail.com>
2019-10-20 23:02:36 -0400

    Added hash links to each education item

--- ClientApp/src/app/resume/education/models/timeline-item.model.ts
+++ ClientApp/src/app/resume/education/models/timeline-item.model.ts
@@ -2,13 +2,15 @@
 
 export class TimelineItem {
   title: string;
+  name: string;
   startTime: string;
   endTime: string;
   logoFileName: string;
   description: string;
 
-  constructor(title: string, startTime: string, endTime: string, logoFileName: string, description: string) {
+  constructor(title: string, name: string, startTime: string, endTime: string, logoFileName: string, description: string) {
     this.title = title;
+    this.name = name;
     this.startTime = startTime;
     this.endTime = endTime;
     this.logoFileName = logoFileName;
